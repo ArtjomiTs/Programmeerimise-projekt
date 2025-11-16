17c142c5decb50d5cc629e402994b7e9aeed3c57
MT999MT <mihkel.tomson2006@gmail.com>
2025-11-16 02:30:21 +0200

    uuendatud lehe struktuuri, lisatud esialgne drop-down menüü.

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# /// Avaleht (index.html)
+# /// Avaleht (avaleht.html)
 @app.route('/')
 def avaleht():
-    return render_template('index.html')
+    return render_template('avaleht.html')
 
 # /// Tagasiside leht (tagasiside.html)
 @app.route('/tagasiside', methods=['GET', 'POST'])
@@ -37,6 +37,16 @@ def tagasiside():
 @app.route('/aitah')
 def aitah():
     return '<h2>Aitäh tagasiside eest!</h2><a href="/">Tagasi avalehele</a>'
+
+# /// profiili leht (profiil.html)
+@app.route ('/profiil')
+def profiil():
+    return render_template('profiil.html')
+
+# /// Postkasti leht (postkast.html)
+@app.route ('/postkast.html')
+def postkast():
+    return render_template('postkast.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
